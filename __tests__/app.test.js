681a826fd3a3aa23055a4be1af8f7da47a747344
jdane <julien.dane@gmail.com>
2022-09-27 11:43:34 +0200

    Program implemented.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,14 @@
 const app = require("../app.js");
 
+describe("arabToRomanDigit", () => {
+  it("should return [0, 'M']", () => {
+    expect(app.arabToRomanDigit(1000, 1000, "")).toEqual([0, "M"]);
+  });
+  it("should return [1, 'C']", () => {
+    expect(app.arabToRomanDigit(100, 101, "")).toEqual([1, "C"]);
+  });
+});
+
 describe("convertToRoman", () => {
   it("should return the string 'II'", () => {
     expect(app.convertToRoman(2)).toEqual("II");
@@ -9,5 +18,35 @@ describe("convertToRoman", () => {
   });
   it("should return the string 'IV'", () => {
     expect(app.convertToRoman(4)).toEqual("IV");
+  });
+  it("should return the string 'MM'", () => {
+    expect(app.convertToRoman(2000)).toEqual("MM");
+  });
+  it("should return the string 'CM'", () => {
+    expect(app.convertToRoman(900)).toEqual("CM");
+  });
+  it("should return the string 'D'", () => {
+    expect(app.convertToRoman(500)).toEqual("D");
+  });
+  it("should return the string 'CD'", () => {
+    expect(app.convertToRoman(400)).toEqual("CD");
+  });
+  it("should return the string 'DCC'", () => {
+    expect(app.convertToRoman(700)).toEqual("DCC");
+  });
+  it("should return the string 'D'", () => {
+    expect(app.convertToRoman(500)).toEqual("D");
+  });
+  it("should return the string 'CD'", () => {
+    expect(app.convertToRoman(400)).toEqual("CD");
+  });
+  it("should return the string 'CCC'", () => {
+    expect(app.convertToRoman(300)).toEqual("CCC");
+  });
+  it("should return the string 'XII'", () => {
+    expect(app.convertToRoman(12)).toEqual("XII");
+  });
+  it("should return the string 'XVI'", () => {
+    expect(app.convertToRoman(16)).toEqual("XVI");
   });
 });
--- app.js
+++ app.js
@@ -1,7 +1,90 @@
+function arabToRomanDigit(value, n, roman) {
+  // substract value from n and concatenate roman digit to roman string
+  const ROMAN_DIGITS = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I",
+  };
+
+  return [n - value, roman.concat(ROMAN_DIGITS[value])];
+}
+
 function convertToRoman(n) {
-  return "I".repeat(n);
+  
+
+  let roman = "";
+
+  // thousands
+  while (n >= 1000) {
+    [n, roman] = arabToRomanDigit(1000, n, roman);
+  }
+
+  // hundreds
+  if (n >= 900) {
+    [n, roman] = arabToRomanDigit(900, n, roman);
+  }
+
+  if (n >= 500) {
+    [n, roman] = arabToRomanDigit(500, n, roman);
+  }
+
+  if (n >= 400) {
+    [n, roman] = arabToRomanDigit(400, n, roman);
+  }
+
+  while (n >= 100) {
+    [n, roman] = arabToRomanDigit(100, n, roman);
+  }
+
+  // tens
+
+  if (n >= 90) {
+    [n, roman] = arabToRomanDigit(90, n, roman);
+  }
+
+  if (n >= 50) {
+    [n, roman] = arabToRomanDigit(50, n, roman);
+  }
+
+  if (n >= 40) {
+    [n, roman] = arabToRomanDigit(40, n, roman);
+  }
+
+  while (n >= 10) {
+    [n, roman] = arabToRomanDigit(10, n, roman);
+  }
+
+  // units
+  if (n >= 9) {
+    [n, roman] = arabToRomanDigit(9, n, roman);
+  }
+
+  if (n >= 5) {
+    [n, roman] = arabToRomanDigit(5, n, roman);
+  }
+
+  if (n === 4) {
+    [n, roman] = arabToRomanDigit(4, n, roman);
+  }
+  
+  while (n > 0) {
+    [n, roman] = arabToRomanDigit(1, n, roman);
+  }
+
+  return roman;
 }
 
 module.exports = {
+  arabToRomanDigit,
   convertToRoman
 }
